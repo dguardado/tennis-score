88b56f09e7aa56f1a8ee80f76e5150b76c4e1764
Dimas Guardado <me@dimas.tech>
2019-02-12 21:47:27 -0800

    Initialize a basic project

---
+++ src/main/java/tech/dimas/cfive/App.java
@@ -0,0 +1,7 @@
+package tech.dimas.cfive;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/tech/dimas/cfive/AppTest.java
@@ -0,0 +1,12 @@
+package tech.dimas.cfive;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertThat(true).isTrue();
+    }
+}
